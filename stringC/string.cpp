0df43811b6aa72282a53de827e192e84e0060689
PengfeiHou <837282831@qq.com>
2026-09-07 08:54:47 +0800

    自定义string类

---
+++ stringC/string.cpp
@@ -0,0 +1,99 @@
+#include "string.h"
+
+void string::realloccate(size_t new_capacity)
+{
+    // Allocate new memory for the string with the specified new capacity
+    // 最少分配min_capacity个字节的内存，保证字符串的最小容量
+    new_capacity = (new_capacity < min_capacity) ? min_capacity : new_capacity;
+    char* new_data = new char[new_capacity];
+
+    if(size_ > 0) {
+        // Copy the existing data to the new memory
+        std::memcpy(new_data, data_, size_);//同时支持c字符串和二进制安全字符串，赋值时把字符串中包含的/0也复制过去
+    }
+
+    //capacity和size都不包含\0，size是实际字符串的长度，capacity是实际分配的内存大小
+    //实际分配的大小是new_capacity+1，用于存储字符串结束符\0
+    new_data[size_] = '\0';  // 结尾添加/0，保证字符串的正确性
+
+    // Deallocate the old memory
+    delete[] data_;
+
+    // Update the data pointer and capacity
+    data_ = new_data;
+    capacity_ = new_capacity;
+
+}
+
+string::string(): size_(0), capacity_(min_capacity)
+{
+    data_ = new char[capacity_+1];  // Allocate memory for the string, including space for the null terminator
+    data_[0] = '\0';  // Initialize the string as an empty string
+}
+
+string::~string()
+{
+    delete [] data_;  // Deallocate the memory used for the string
+}
+
+string::string(const char *str)
+{
+    if(nullptr == str) {
+        throw std::invalid_argument("Null pointer");
+    } else {
+        size_ = std::strlen(str);
+        capacity_ = (size_ < min_capacity) ? min_capacity : size_;//最小取15，保证字符串的最小容量
+        data_ = new char[capacity_ + 1];
+        std::memcpy(data_, str, size_ + 1);  // Copy the null terminator as well
+    }
+}
+
+string::string(const void *data, size_t len)
+{
+    if(nullptr == data) {
+        throw std::invalid_argument("Null pointer");
+    } else {
+        size_ = len;
+        capacity_ = (size_ < min_capacity) ? min_capacity : size_;//最小取15，保证字符串的最小容量
+        data_ = new char[capacity_ + 1];
+        std::memcpy(data_, data, len);
+        data_[size_] = '\0';//不管是c字符串还是二进制数据，结尾都加上/0，保证字符串的正确性
+    }
+}
+
+string::string(const string &other):size_(other.size_), capacity_(other.capacity_)
+{
+    data_ = new char[capacity_ + 1];
+    std::memcpy(data_, other.data_, size_ + 1);  // Copy the null terminator as well
+}
+
+//移动赋值运算符
+string::string(string&& other) noexcept
+    : data_(other.data_), size_(other.size_), capacity_(other.capacity_)
+{
+    other.data_ = nullptr;
+    other.size_ = 0;
+    other.capacity_ = 0;
+}
+
+//拷贝赋值运算符
+string &string::operator=(const string &other)
+{
+    if(this != &other) {
+        char* new_data = new char[other.capacity_ + 1];
+        std::memcpy(new_data, other.data_, other.size_ + 1);
+        delete[] data_;
+        data_ = new_data;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+    }
+    return *this;
+}
+
+void string::reserve(size_t new_capacity)
+{
+}
+
+void string::shrink_to_fit()
+{
+}
---
+++ stringC/string.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+
+class string
+{
+public:
+    static const size_t min_capacity;  // Minimum capacity for the string
+
+
+private:
+    char* data_;  // Pointer to the character array
+    size_t size_;  // Current size of the string
+    size_t capacity_;  // Current capacity of the string
+
+private:
+    void realloccate(size_t new_capacity);  // Function to reallocate memory for the string
+
+
+
+
+
+public:
+    string();//默认构造函数
+    ~string();
+
+    string(const char* str);  // 从c风格字符串构造
+    string(const void* data, size_t len);  // 从二进制数据构造
+    string(const string& other);  // Copy constructor
+    //noexcept表示这个函数不会抛出异常，移动构造函数通常不会抛出异常，因为它只是转移资源的所有权，而不是分配新的资源。这样可以提高性能，尤其是在容器中使用时。
+    string(string&& other) noexcept;  // Move constructor,移动构造
+    string& operator=(const string& other);  // Copy assignment operator
+
+    //预分配内存
+    void reserve(size_t new_capacity);  // Reserve memory for the string
+    //释放多余内存
+    void shrink_to_fit();  // Shrink the capacity to fit the size
+    
+
+
+
+};
+
+const size_t string::min_capacity = 15;  // Initialize the minimum capacity
